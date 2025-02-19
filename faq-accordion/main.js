8e48c121ed92b880269ad3ec92813f7e472e8fb0
Hamza Mirza <hamza665@hotmail.de>
2025-02-19 14:18:34 +0100

    added functionality to open and close faq

--- faq-accordion/main.js
+++ faq-accordion/main.js
@@ -3,15 +3,31 @@ import { questionsAnswers } from "./utils/questionAnswer.js";
 const faqList = document.querySelector(".faq-list");
 
 questionsAnswers.forEach((el) => {
-  faqList.innerHTML += `<li class="question-item">
+  faqList.innerHTML += `<li class="question-item" id=${el.id}>
                     <div class="question-icon">
                         <p class="question">${el.question}</p>
-                        <span class="material-symbols-rounded open-icon">
-                            add_circle
+                        <span class="material-symbols-rounded toggle-icon">
+                            expand_circle_down
                         </span>
                     </div>
                     <p class="answer closed">${el.answer}</p>
                 </li>`;
 });
 
-const closeBtns = document.querySelectorAll(".open-icon");
+const closeBtns = document.querySelectorAll(".toggle-icon");
+const answer = document.querySelectorAll(".answer");
+
+closeBtns.forEach((btn) => {
+  btn.addEventListener("click", (el) => {
+    const parentEl = el.target.closest(".question-item");
+    const answer = parentEl.querySelector(".answer");
+    const icon = parentEl.querySelector(".toggle-icon");
+
+    answer.classList.toggle("closed");
+    if (answer.classList.contains("closed")) {
+      icon.innerText = "expand_circle_down";
+    } else {
+      icon.innerText = "expand_circle_up";
+    }
+  });
+});
--- faq-accordion/utils/questionAnswer.js
+++ faq-accordion/utils/questionAnswer.js
@@ -3,24 +3,24 @@ export const questionsAnswers = [
     id: 1,
     question: "What is Frontend Mentor, and how will it help me?",
     answer:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, repudiandae, laudantium ducimus reiciendis atque, amet dolore distinctio error totam sit esse fugit voluptatum quibusdam ullam nulla accusantium architecto impedit iste.",
+      "Coffee window bright silent clock running forward, table glass morning breeze enters softly. Thoughts wander endlessly, dreaming distant places unknown. Shadows stretch, pages turn, whispered echoes remain, forgotten melodies hum beneath silver moonlight still.",
   },
   {
     id: 2,
     question: "Is Frontend Mentor free?",
     answer:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, repudiandae, laudantium ducimus reiciendis atque, amet dolore distinctio error totam sit esse fugit voluptatum quibusdam ullam nulla accusantium architecto impedit iste.",
+      "Laptop flickers, screen glowing dim, waiting commands typed hurriedly. Streetlights blink, distant sirens wail, midnight thoughts race. Raindrops scatter sidewalk reflections, footsteps fade, memories linger, empty hallways echo voices lost, whispers unheard.",
   },
   {
     id: 3,
     question: "Can I use Frontend Mentor projects in my portfolio?",
     answer:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, repudiandae, laudantium ducimus reiciendis atque, amet dolore distinctio error totam sit esse fugit voluptatum quibusdam ullam nulla accusantium architecto impedit iste.",
+      "Door creaks, cat jumps, radio plays soft tunes. Distant laughter floats, candle flickers, curtains sway gently. Forgotten letters pile dusty corners, heartbeats steady, morning sun peeks, yesterday fades, today whispers possibilities.",
   },
   {
     id: 4,
     question: "How can I get help if I'm stuck on a challenge?",
     answer:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, repudiandae, laudantium ducimus reiciendis atque, amet dolore distinctio error totam sit esse fugit voluptatum quibusdam ullam nulla accusantium architecto impedit iste.",
+      "Clock ticks, pen scribbles, notebook fills slowly. Chair wobbles, tea steams, wind howls outside. Thoughts jumble, sentences flow, ideas form. Music plays softly, shadows stretch walls, night deepens, silence lingers.",
   },
 ];
