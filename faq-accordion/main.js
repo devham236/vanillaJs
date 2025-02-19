bbced381a78a32a96bfcb402a1c81f61a493190c
Hamza Mirza <hamza665@hotmail.de>
2025-02-19 12:53:48 +0100

    hide answer on default

--- faq-accordion/main.js
+++ faq-accordion/main.js
@@ -6,10 +6,12 @@ questionsAnswers.forEach((el) => {
   faqList.innerHTML += `<li class="question-item">
                     <div class="question-icon">
                         <p class="question">${el.question}</p>
-                        <span class="material-symbols-rounded">
+                        <span class="material-symbols-rounded open-icon">
                             add_circle
                         </span>
                     </div>
-                    <p class="answer">${el.answer}</p>
+                    <p class="answer closed">${el.answer}</p>
                 </li>`;
 });
+
+const closeBtns = document.querySelectorAll(".open-icon");
